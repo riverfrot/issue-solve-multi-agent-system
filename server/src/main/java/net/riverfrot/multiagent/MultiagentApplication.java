825f9de2e497cdec28570dc50a5fbfdf8c4f258d
riverfrot <riverfrot@gmail.com>
2025-12-02 06:20:09 +0900

    spring backend add and change agent

---
+++ server/src/main/java/net/riverfrot/multiagent/MultiagentApplication.java
@@ -0,0 +1,13 @@
+package net.riverfrot.multiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiagentApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/net/riverfrot/multiagent/MultiagentApplicationTests.java
@@ -0,0 +1,13 @@
+package net.riverfrot.multiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiagentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
